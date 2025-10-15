6c0e26fb3b9f48dfd73e95942fc0bf8a79a7d186
Devender Kumar <devender00999@gmail.com>
2025-10-15 23:33:47 +0530

    Started swap adjacent nodes

--- linked-list/LinkedList.py
+++ linked-list/LinkedList.py
@@ -524,6 +524,11 @@ class MyLinkedList(object):
       newHead = s.next
       s.next = None
       return newHead
+   
+   # Given a linked list, swap every two adjacent nodes and return its head. 
+   # You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+   def swapPair(head):
+      if not head or not head.next: return head
 
    
 
