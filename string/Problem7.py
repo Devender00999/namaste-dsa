ba91e9bf5da5eecfe98a06a2b14e10772ca99717
Devender Kumar <devender00999@gmail.com>
2025-10-27 23:44:41 +0530

    Added Reverse a string with start and end. def reverse(s, start, end)

--- string/Problem7.py
+++ string/Problem7.py
@@ -1,12 +1,16 @@
-# Palindrome
-def isPalindrome(self, s):
-   """
-   :type s: str
-   :rtype: bool
-   """
-   nstr = ''
-   for i in s.lower():
-      if i >= 'a' and i <= 'z' or i >= '0' and i <= '9':
-            nstr += i
-   print(nstr)
-   return nstr == nstr[::-1]
+# Reverse a string with start and end.
+def reverse(s, start, end):
+   length = len(s)
+   if start < 0 or end >= length: return s
+   rev = ''
+   if start > 0:
+      rev += s[:start ]
+   for i in range(end, start - 1, -1):
+      print(i)
+      rev += s[i]
+   if end < len(s):
+      rev += s[end + 1: len(s)]
+   return rev
+
+
+print(reverse('hello', 1, 3))
