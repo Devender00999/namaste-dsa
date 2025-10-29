a212fdcc584a194471ef5c922b52958353329d34
Devender Kumar <devender00999@gmail.com>
2025-10-29 10:58:07 +0530

    Added alternative approach for palindrome.

--- string/Problem8.py
+++ string/Problem8.py
@@ -1,4 +1,11 @@
-# Palindrome
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
+# Alphanumeric characters include letters and numbers.
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+
+# T(n): O(n), S(n): O(n)
 def isPalindrome(self, s):
    """
    :type s: str
@@ -8,5 +15,27 @@ def isPalindrome(self, s):
    for i in s.lower():
       if i >= 'a' and i <= 'z' or i >= '0' and i <= '9':
             nstr += i
-   print(nstr)
    return nstr == nstr[::-1]
+
+# T(n): O(n), S(n): O(n)
+def isPalindromeStringV2(self, s):
+   nstr = ''
+   for i in s.lower():
+      if i >= 'a' and i <= 'z' or i >= '0' and i <= '9':
+            nstr += i
+            rev = i + rev
+   n = len(nstr)
+   for i in range(n//2):
+       if (nstr[i] != nstr[n-1-i]):
+           return False
+   return True
+
+# T(n): O(n), S(n): O(n)
+def isPalindromeStringV3(self, s):
+   nstr = ''
+   rev = ''
+   for i in s.lower():
+      if i >= 'a' and i <= 'z' or i >= '0' and i <= '9':
+            nstr += i
+            rev = i + rev
+   return rev == nstr
