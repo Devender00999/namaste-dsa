e2e4b7989ed18648e6397706a05f3966119751d1
Devender Kumar <devender00999@gmail.com>
2025-10-30 21:03:52 +0530

    Added efficient approach for palindrome

--- string/Problem8.py
+++ string/Problem8.py
@@ -6,7 +6,7 @@
 # Explanation: "amanaplanacanalpanama" is a palindrome.
 
 # T(n): O(n), S(n): O(n)
-def isPalindrome(self, s):
+def isPalindromeString(s):
    """
    :type s: str
    :rtype: bool
@@ -18,12 +18,11 @@ def isPalindrome(self, s):
    return nstr == nstr[::-1]
 
 # T(n): O(n), S(n): O(n)
-def isPalindromeStringV2(self, s):
+def isPalindromeStringV2(s):
    nstr = ''
    for i in s.lower():
       if i >= 'a' and i <= 'z' or i >= '0' and i <= '9':
             nstr += i
-            rev = i + rev
    n = len(nstr)
    for i in range(n//2):
        if (nstr[i] != nstr[n-1-i]):
@@ -31,7 +30,7 @@ def isPalindromeStringV2(self, s):
    return True
 
 # T(n): O(n), S(n): O(n)
-def isPalindromeStringV3(self, s):
+def isPalindromeStringV3(s):
    nstr = ''
    rev = ''
    for i in s.lower():
@@ -39,3 +38,28 @@ def isPalindromeStringV3(self, s):
             nstr += i
             rev = i + rev
    return rev == nstr
+
+def isAlphaNum(i):
+      return i >= 'a' and i <= 'z' or i >= '0' and i <= '9'
+
+# T(n): O(n), S(n): O(1)
+def isPalindromeStringV4(s):
+   start = 0
+   end = len(s) - 1
+   s = s.lower()
+   while start < end:
+      if not isAlphaNum(s[start]):
+         start += 1
+      elif not isAlphaNum(s[end]):
+         end -= 1
+      elif s[start] == s[end]:
+         start += 1
+         end -= 1
+      else:
+         return False
+   return True
+
+print(isPalindromeStringV4('A man, a plan, a canal: Panama'))
+# print(isPalindromeString('A man, a plan, a canal: Panama'))
+# print(isPalindromeStringV2('A man, a plan, a canal: Panama'))
+# print(isPalindromeStringV3('A man, a plan, a canal: Panama'))
