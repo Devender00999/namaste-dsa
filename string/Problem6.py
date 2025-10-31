6dca3226b1f2bcd95cba2de21b76aba10f4e5432
Devender Kumar <devender00999@gmail.com>
2025-10-31 22:35:09 +0530

    Added improved version for largest odd number.

--- string/Problem6.py
+++ string/Problem6.py
@@ -5,10 +5,19 @@
 # Output: "5"
 # Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
 
-def largestOddNumber(self, num):
+def largestOddNumberV1(num):
    last_odd = -1
    for i in range(len(num) - 1, -1, -1):
       if int(num[i]) % 2 != 0:
             last_odd = i
-            break
-   return num[:last_odd + 1] if last_odd != -1 else ""
+            return num[:last_odd + 1]
+   return ""
+
+def largestOddNumberV2(num):
+   for i in range(len(num) - 1, -1, -1):
+      if int(num[i]) % 2 != 0:
+         return num[:i+1]
+   return ""
+
+print(largestOddNumberV1('5232'))
+print(largestOddNumberV2('5232'))
