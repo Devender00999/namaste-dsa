31cd26fdaef912c03d8b98d88b4483df403161d5
Devender Kumar <devender00999@gmail.com>
2025-11-10 23:26:21 +0530

    Added stack from two queues

---
+++ stack-queues/Problem1.py
@@ -0,0 +1,45 @@
+# Stack 
+
+class MyStack:
+   def __init__(self):
+      self.queue = []
+      self.helper = []
+      self.topIdx = -1
+      
+
+   def push(self, x: int) -> None:
+      self.queue.append(x)
+      self.topIdx += 1
+
+   def pop(self) -> int:
+      l = len(self.queue)
+      for i in range(l - 1):
+         self.helper.append(self.queue.pop(0))
+      item = self.queue.pop(0)
+      self.queue = self.helper
+      self.helper = []
+      self.topIdx -= 1
+      return item
+               
+
+   def top(self) -> int:
+      # return self.queue[self.topIdx]
+      l = len(self.queue)
+      for i in range(l - 1):
+         self.helper.append(self.queue.pop(0))
+      last = self.queue.pop(0)
+      self.helper.append(last)
+      self.queue = self.helper
+      self.helper = []
+      return last
+      
+
+   def empty(self) -> bool:
+      return self.topIdx == -1
+   
+stack = MyStack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.pop())
+# print(stack.top())
