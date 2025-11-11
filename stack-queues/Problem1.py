48acc0afe36d5336b9ad551a6ef5614e8bca600f
Devender Kumar <devender00999@gmail.com>
2025-11-11 22:52:25 +0530

    Added queue using stack

--- stack-queues/Problem1.py
+++ stack-queues/Problem1.py
@@ -37,6 +37,31 @@ class MyStack:
    def empty(self) -> bool:
       return self.topIdx == -1
    
+class MyStack:
+   def __init__(self):
+      self.queue = []
+
+   def push(self, x: int) -> None:
+      self.queue.append(x)
+
+   def pop(self) -> int:
+      l = len(self.queue)
+      for i in range(l - 1):
+         self.queue.append(self.queue.pop(0))
+      return self.queue.pop(0)
+
+   def top(self) -> int:
+      l = len(self.queue)
+      for i in range(l - 1):
+         self.queue.append(self.queue.pop(0))
+      last = self.queue.pop(0)
+      self.queue.append(last)        
+      return last
+      
+   def empty(self) -> bool:
+      return len(self.queue) == 0
+
+   
 stack = MyStack()
 stack.push(1)
 stack.push(2)
