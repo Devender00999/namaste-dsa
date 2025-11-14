e004f432c12def243197b605b3777d6306932210
Devender Kumar <devender00999@gmail.com>
2025-11-14 23:52:07 +0530

    Added alternate approach

--- stack-queues/Problem4.py
+++ stack-queues/Problem4.py
@@ -1,5 +1,6 @@
 # Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
+# T(n): O(1) for all, S(n): O(n)
 class MinStack:
    def __init__(self):
       self.st1 = []
@@ -27,4 +28,31 @@ class MinStack:
    def getMin(self) -> int:
       if len(self.st2) > 0:
          return self.st2[len(self.st2) - 1]
+      
+
+# T(n): O(1) for all, S(n): O(1)
+class MinStack:
+   def __init__(self):
+        self.st1 = []
+        
+   def push(self, val: int) -> None:
+      if len(self.st1) == 0:
+         self.st1.append({ "value": val, "min": val})
+      else:
+         top = len(self.st1) - 1
+         if self.st1[top]["min"] <= val:
+               self.st1.append({ "value": val, "min": self.st1[top]['min']})
+         else:
+               self.st1.append({ "value": val, "min": val})
+
+   def pop(self) -> None:
+      self.st1.pop()
+
+   def top(self) -> int:
+      return self.st1[len(self.st1) - 1]['value']
+
+   def getMin(self) -> int:
+      if len(self.st1) > 0:
+         return self.st1[len(self.st1) - 1]['min']
+
       
