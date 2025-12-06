4f57dab9def82d6b7a955f5db942cd6dbf3bd4f5
Devender Kumar <devender00999@gmail.com>
2025-12-07 00:03:53 +0530

    Added peak index in mountain array.

--- binary-search-algorithms/Problem8.py
+++ binary-search-algorithms/Problem8.py
@@ -1,12 +1,19 @@
+from typing import List
+
+# You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+# Input: arr = [0,1,0]
+# Output: 1
+
 def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        l = 0; r = len(arr) - 1
-        max = -1
-        while l < r:
-            m = l + (r - l) // 2
-            if arr[m - 1] < arr[m] and arr[m + 1] < arr[m]:
-                return m
-            elif arr[m] < arr[m + 1]:
-                l = m
-            else:
-                r = m
-        return max
+   l = 0; r = len(arr) - 1
+   max = -1
+   while l < r:
+      m = l + (r - l) // 2
+      if arr[m - 1] < arr[m] and arr[m + 1] < arr[m]:
+            return m
+      elif arr[m] < arr[m + 1]:
+            l = m
+      else:
+            r = m
+   return max
