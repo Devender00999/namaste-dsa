16a44f941160cce2616ae66de5b8674092587599
Devender Kumar <devender00999@gmail.com>
2025-12-10 23:24:13 +0530

    added optimized version for closest element.

--- binary-search-algorithms/Problem10.py
+++ binary-search-algorithms/Problem10.py
@@ -6,9 +6,23 @@ from typing import List
 # Input: arr = [1,2,3,4,5], k = 4, x = 3
 # Output: [1,2,3,4]
 
+# T(n): O(n)
+# S(n): O(1)
 def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
    i = 0
    while i + k < len(arr) and  (x - arr[i] > arr[i + k] - x):
       i += 1
    print(i)
    return arr[i:i + k]
+
+# T(n): O(logn)
+# S(n): O(1)
+def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+   l = 0; r = len(arr) - 1
+   while l < r:
+      m = l + (r - l) // 2
+      if m + k < len(arr) and (arr[m + k] - x) < (x - arr[m]):
+            l = m + 1
+      else:
+            r = m
+   return arr[r: r+k]
