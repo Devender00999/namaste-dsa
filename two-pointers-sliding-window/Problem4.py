d7fdc24e5b329f2210c510df0c2bc1e22f7d9803
Devender Kumar <devender00999@gmail.com>
2025-12-17 14:30:00 +0530

    Added approach 2 for finding first occurance in string

--- two-pointers-sliding-window/Problem4.py
+++ two-pointers-sliding-window/Problem4.py
@@ -1,16 +1,34 @@
 # Index of the First Occurrence in a String
-def strStr(self, h: str, n: str) -> int:
+
+# Input: haystack = "sadbutsad", needle = "sad"
+# Output: 0
+
+# T(n): O(n*m); S(n): O(1)
+def strStr(self, haystack: str, needle: str) -> int:
    i = 0; 
-   for i in range(len(h)):
+   for i in range(len(haystack)):
       j = 0
-      if h[i] == n[j]:
+      if haystack[i] == needle[j]:
             x = i + 1
             y = j + 1
-            while x  < len(h) and y < len(n):
-               if h[x] != n[y]:
+            while x  < len(haystack) and y < len(needle):
+               if haystack[x] != needle[y]:
                   break
                x += 1
                y += 1
-            if y == len(n):
+            if y == len(needle):
                return i
+   return -1
+
+# T(n): O(n*m); S(n): O(1)
+def strStr(haystack: str, needle: str) -> int:
+   m, n = len(haystack), len(needle)
+   for i in range(m - n + 1):
+      j = 0
+      while j < n:
+            if haystack[i + j] != needle[j]:
+               break
+            j += 1
+      if j == n:
+            return i
    return -1
