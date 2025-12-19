8a8d54f9484061eaf8edb063bfef22009edb30c7
Devender Kumar <devender00999@gmail.com>
2025-12-19 14:30:00 +0530

    Added KMP Algorithm for string matching

--- two-pointers-sliding-window/Problem4.py
+++ two-pointers-sliding-window/Problem4.py
@@ -33,4 +33,35 @@ def strStr(haystack: str, needle: str) -> int:
             return i
    return -1
 
+def strStr(haystack: str, needle: str) -> int:
+   m, n = len(haystack), len(needle)
+   i = 0; j = 1; lps = [0]
+   while j < n:
+      if needle[i] == needle[j]:
+            lps.append(i + 1)
+            i += 1
+            j += 1
+      else:
+            if (i == 0):
+               lps.append(0)
+               j += 1
+            else:
+               i = lps[i - 1]
+
+   j = 0; i = 0
+   while (i < m):      
+      print(i, j)
+      if haystack[i] == needle[j]:
+            j += 1
+            i += 1
+      else:
+            if j == 0:
+               i+= 1
+            else:
+               j = lps[j - 1]
+            
+      if j == n:
+            return i - n
+   return -1
+   
 # KMP algorithm
