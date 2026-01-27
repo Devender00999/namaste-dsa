97025c8af04d6db6b7fe3ca8f17d78dcba64c50d
Devender Kumar <devender00999@gmail.com>
2026-01-27 22:55:28 +0530

    Added level order traversal using queue

---
+++ 10. trees/levelOrder.py
@@ -0,0 +1,57 @@
+from Tree import TreeNode
+from typing import List
+
+# Example
+# Create nodes
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node4 = TreeNode(4)
+node5 = TreeNode(5)
+node6 = TreeNode(6)
+node7 = TreeNode(7)
+node8 = TreeNode(8)
+node9 = TreeNode(9)
+
+# Build the tree
+node1.left = node2
+node1.right = node3
+
+node2.left = node4
+node2.right = node5
+
+node5.left = node6
+node5.right = node7
+
+node3.right = node8
+node8.left = node9
+
+root = node1
+
+def levelOrderWithArray(root: TreeNode):
+   ans = []
+   if not root: return ans
+   q1 = [root]
+   while len(q1):
+      levelArr = []
+      for i in range(len(q1)):
+         curr = q1.pop(0)
+         levelArr.append(curr.val)
+         curr.left and q1.append(curr.left)
+         curr.right and q1.append(curr.right)
+      ans.append(levelArr)
+   return ans
+
+def levelOrder(root: TreeNode):
+   ans = []
+   if not root: return ans
+   q1 = [root]
+   while len(q1):
+      curr = q1.pop(0)
+      ans.append(curr.val)
+      curr.left and q1.append(curr.left)
+      curr.right and q1.append(curr.right)
+   return ans
+
+print(levelOrderWithArray(root))
+print(levelOrder(root))
