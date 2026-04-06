ba57c7fe2b891880d6d85fe0f8f72f6e0364d651
Devender Kumar <devender00999@gmail.com>
2026-04-06 08:32:43 +0530

    Added extract in heap

--- 12. heap-priority-queue/heap.py
+++ 12. heap-priority-queue/heap.py
@@ -16,6 +16,12 @@ class MinHeap:
       self.heap.append(elem)
       self.heapifyUp(len(self.heap) - 1)
       
+   def extract(self):
+      if (len(self.heap) == 0): return
+      self.heap[0] = self.heap[len(self.heap) - 1]
+      self.heap.pop()
+      self.heapifyDown(0)
+      
    def heapifyUp(self, i):
       while i > 0:
          parentIdx = self.getParentIndex(i)
@@ -25,6 +31,21 @@ class MinHeap:
          else:
             break
          
+   def heapifyDown(self, i = 0):
+      left = self.getLeftChildIndex(i)
+      right = self.getRightChildIndex(i);
+      n = len(self.heap)
+      smallest = i
+      if (left < n and self.heap[left] < self.heap[smallest]):
+         smallest = left
+      if (right < n and self.heap[right] < self.heap[smallest]):
+         smallest = right
+      
+      if (smallest != i):
+         self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+         self.heapifyDown(smallest)
+      
+               
 class MaxHeap:
    def __init__(self):
       self.heap = []
@@ -42,6 +63,12 @@ class MaxHeap:
       self.heap.append(elem)
       self.heapifyUp(len(self.heap) - 1)
       
+   def extract(self):
+      if (len(self.heap) == 0): return
+      self.heap[0] = self.heap[len(self.heap) - 1]
+      self.heap.pop()
+      self.heapifyDown(0)
+      
    def heapifyUp(self, i):
       while i > 0:
          parentIdx = self.getParentIndex(i)
@@ -50,13 +77,32 @@ class MaxHeap:
             i = parentIdx
          else:
             break
+   def heapifyDown(self, i = 0):
+      left = self.getLeftChildIndex(i)
+      right = self.getRightChildIndex(i);
+      n = len(self.heap)
+      greatest = i
+      if (left < n and self.heap[left] > self.heap[greatest]):
+         greatest = left
+      if (right < n and self.heap[right] > self.heap[greatest]):
+         greatest = right
+      
+      if (greatest != i):
+         self.heap[i], self.heap[greatest] = self.heap[greatest], self.heap[i]
+         self.heapifyDown(greatest)
+
 
 heap = MaxHeap()
 
 heap.insert(5)
+heap.insert(30)
 heap.insert(10)
 heap.insert(20)
-heap.insert(30)
-heap.insert(1)
-heap.insert(31)
+
+print(heap.heap)
+heap.extract()
+# heap.extract()
+# heap.extract()
+# heap.extract()
+# heap.extract()
 print(heap.heap)
