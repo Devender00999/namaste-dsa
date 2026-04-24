94f37c69d73b10ac9a8582c0cf3555ceff63a8e9
Devender Kumar <devender00999@gmail.com>
2026-04-24 08:29:31 +0530

    Added 2nd variation for combination sum 2

--- 13. backtracking/combination-sum-2.js
+++ 13. backtracking/combination-sum-2.js
@@ -17,3 +17,24 @@ var combinationSum2 = function (nums, target) {
    backtrack([], 0);
    return res;
 };
+
+// second variation
+var combinationSum2 = function (nums, target) {
+   const res = [];
+   nums.sort();
+   function backtrack(path, start, currTarget) {
+      if (currTarget == 0) {
+         res.push([...path]);
+      }
+      if (currTarget < 0) return;
+
+      for (let i = start; i < nums.length; i++) {
+         if (start < i && nums[i] == nums[i - 1]) continue;
+         path.push(nums[i]);
+         backtrack(path, i + 1, currTarget - nums[i]);
+         path.pop();
+      }
+   }
+   backtrack([], 0, target);
+   return res;
+};
