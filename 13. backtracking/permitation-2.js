7c950b6ef6eab5e94c7b9f98dda9e364c3e38093
Devender Kumar <devender00999@gmail.com>
2026-04-27 00:29:42 +0530

    Added permutation II

---
+++ 13. backtracking/permitation-2.js
@@ -0,0 +1,19 @@
+var permuteUnique = function (nums) {
+   let ans = [];
+   const backtrack = (path, choices) => {
+      if (path.length == nums.length) {
+         ans.push([...path]);
+         return;
+      }
+
+      for (let i = 0; i < choices.length; i++) {
+         if (i > 0 && choices[i] == choices[i - 1]) continue;
+         path.push(choices[i]);
+         backtrack(path, [...choices.slice(0, i), ...choices.slice(i + 1)]);
+         path.pop();
+      }
+   };
+   nums = nums.sort();
+   backtrack([], nums);
+   return ans;
+};
