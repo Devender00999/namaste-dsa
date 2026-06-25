2d3c772cfe94ab972b2d474d914961cbcff3f9cc
Devender Kumar <devender00999@gmail.com>
2026-06-25 23:08:07 +0530

    Added optimised approach for gas station problem

--- 14. greedy-algorithm/gas-station.js
+++ 14. greedy-algorithm/gas-station.js
@@ -18,3 +18,22 @@ var canCompleteCircuit = function (gas, cost) {
    }
    return -1;
 };
+
+var canCompleteCircuit = function (gas, cost) {
+   let currGain = 0;
+   let totalGain = 0;
+   let ans = 0;
+
+   for (let i = 0; i < gas.length; i++) {
+      let gain = gas[i] - cost[i];
+      currGain = gain + currGain;
+      totalGain += gain;
+
+      if (currGain < 0) {
+         currGain = 0;
+         ans = i + 1;
+      }
+   }
+
+   return totalGain < 0 ? -1 : ans;
+};
