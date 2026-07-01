b1c997747d50b78d54a47e66878f02281a617420
Devender Kumar <devender00999@gmail.com>
2026-07-01 22:03:15 +0530

    Added optimized approach for distribute candies problem.

--- 14. greedy-algorithm/candies.js
+++ 14. greedy-algorithm/candies.js
@@ -20,3 +20,33 @@ var candy = function (ratings) {
    }
    return candies;
 };
+
+var candy = function (ratings) {
+   let n = ratings.length;
+   let ans = n;
+
+   let i = 1;
+   while (i < n) {
+      let up = 0;
+
+      if (ratings[i] == ratings[i - 1]) {
+         i += 1;
+         continue;
+      }
+      while (ratings[i] > ratings[i - 1]) {
+         up += 1;
+         ans += up;
+         i += 1;
+      }
+
+      let down = 0;
+      while (ratings[i] < ratings[i - 1]) {
+         down += 1;
+         ans += down;
+         i += 1;
+      }
+
+      ans -= Math.min(up, down);
+   }
+   return ans;
+};
