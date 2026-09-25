5045e6415088721cdb0f4b75c7bb381627508bae
Devender Kumar <devender00999@gmail.com>
2026-09-25 23:36:52 +0530

    Re-added bottom up for min cost climibing stairs.

--- 15. dynamic-programming/restart/min-cost-climing-stairs.js
+++ 15. dynamic-programming/restart/min-cost-climing-stairs.js
@@ -12,3 +12,11 @@ var minCostClimbingStairs = function (cost) {
    }
    return climb(cost.length);
 };
+
+function minClimbStairs() {
+   const dp = [0, 0];
+   for (let i = 2; i < cost.length; i++) {
+      dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+   }
+   return dp[i];
+}
